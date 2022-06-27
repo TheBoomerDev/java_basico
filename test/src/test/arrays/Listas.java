00ad4df4e8def2cafa89824c051a61a38771def6
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-06-27 17:28:11 +0200

    Map y HashMap

--- test/src/test/arrays/Listas.java
+++ test/src/test/arrays/Listas.java
@@ -38,46 +38,7 @@ public class Listas {
 		
 		Integer value = map.get("valor1");
 		
-		// Partidos de la Semana
-		Map<DIASEMANA, Partido> mapPartidos = new HashMap<DIASEMANA, Partido>();
 		
-		mapPartidos.put(DIASEMANA.LUNES, new Partido());
-		mapPartidos.put(DIASEMANA.MARTES, new Partido());
-		mapPartidos.put(DIASEMANA.MIERCOLES, new Partido());
-		
-		// Sobreescribe el objeto para el Lunes
-		mapPartidos.put(DIASEMANA.LUNES, new Partido());
-		
-		// Más Complicado 
-		Map<DIASEMANA, List<Partido>> muchosPartidos = new HashMap<DIASEMANA, List<Partido>>();
-		
-		List<Partido> partidos = new ArrayList<Partido>();
-		
-		muchosPartidos.put(DIASEMANA.LUNES, partidos);
-		muchosPartidos.put(DIASEMANA.MARTES, partidos);
-		muchosPartidos.put(DIASEMANA.MIERCOLES, partidos); 
-		
-		// Diccionario Mixto
-		Map<String, Object> dictMixto = new HashMap<String, Object>();
-		dictMixto.put("Entero", 121241);
-		dictMixto.put("Texto", "Hola Mundo");
-		dictMixto.put("existe", true);
-		
-		Object obj = dictMixto.get("Entero");
-		Integer jInt = (Integer) obj; 
-		
-		
-		// Diccionario de Diccionarios
-		Map<DIASEMANA, Map<DIASEMANA, List<Partido>>> multiple = new HashMap<DIASEMANA, Map<DIASEMANA, List<Partido>>>();
-		
-		Map<DIASEMANA, List<Partido>> valueMap = new HashMap<DIASEMANA, List<Partido>>();
-		
-		valueMap.put(DIASEMANA.MARTES, new ArrayList<Partido>());
-		
-		multiple.put(DIASEMANA.LUNES, valueMap);
-		
-		
-		System.out.println("FINALIZO");
 	}
 
 }
---
+++ test/src/test/arrays/Matrices.java
@@ -0,0 +1,11 @@
+package test.arrays;
+
+public class Matrices {
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
