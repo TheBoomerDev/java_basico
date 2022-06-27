fdfb74995662a7d02cb40f7123cc52b69493b157
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-06-27 18:25:14 +0200

    Enunciado EJ10

---
+++ test/src/test/ejercicios/ej10.java
@@ -0,0 +1,24 @@
+package test.ejercicios;
+
+public class ej10 {
+	
+	/*
+	 * Hacer un programa de gastos de la casa. 
+		Tipos de Dato: 
+			- Gasto (nombre, fecha, precio)
+			- Ingreso (nombre, fecha, ingreso)
+		Funciones: 
+		- Añadir Ingreso, 
+		- Añadir Gasto, 
+		- Cálculo Gastos en Rango Fechas, 
+		- Cálculo Ingresos en Rango Fechas
+		- Cálculo Balance en Fechas
+
+	 */
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
