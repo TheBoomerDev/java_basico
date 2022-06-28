05b59eb18fb18115408a416f022f8a300ab8d498
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-06-28 17:54:06 +0200

    -> Pistas Sobre Ej10

--- test/src/test/ejercicios/ej10.java
+++ test/src/test/ejercicios/ej10.java
@@ -16,6 +16,24 @@ public class ej10 {
 	 */
 
 	public static void main(String[] args) {
+		
+		/*
+		 *  - Clase Principal => Tendrá 2 Arrays: ingresos, gastos
+		 	- Tipo de Dato => Clases
+		 	- Menu
+		 		- Añadir Ingreso => Igual a Guardar Empleado
+		 		- Añadir Gasto => Igual a Guardar Empleado
+		 		- Cálculo Gastos en Rango Fechas
+		 			=> Filtrar entre fechas -> for + Comprobar .before .after
+		 			=> Usar acumulador
+		 		- Cálculo sIngresos en Rango Fecha
+		 			=> Filtrar entre fechas -> for + Comprobar .before .after
+		 			=> Usar acumulador
+		 		- Cálculo Balance en Fechas
+		 			=> Reusar el código de los 2 anteriores y restar los resultados
+		 
+		 
+		 */
 
 
 	}
