8fe02e58c0125602f5c2e4d7164da8c13e2aa866
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-06-28 16:30:14 +0200

    Visibilidad - Package + Protected

--- test/src/snippets/visibilidad/Visible.java
+++ test/src/snippets/visibilidad/Visible.java
@@ -1,6 +1,7 @@
 package snippets.visibilidad;
 
 public class Visible {
+	// https://www.gotoquiz.com/web-coding/wp-content/uploads/2011/03/java-member-access-levels1.png
 	
 	/**
 	 * Visible Siempre desde fuera de esta clase. 
@@ -18,6 +19,16 @@ public class Visible {
 	 * Solo es accesible desde dentro de la clase
 	 */
 	private int enteroInvisible = 0;
+
+	/**
+	 * Es 'Public' para todas las clases del mismo paquete y las subClases
+	 */
+	protected int enteroProtegido = 0;
+	
+	/**
+	 * Es 'Public' para las clases en el mismo paquete/directorio
+	 */
+	int enteroPackage = 0;
 	
 	
 	public Visible(int entero) {
